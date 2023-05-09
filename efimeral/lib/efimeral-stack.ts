9875426f60ad2862898eacc79ef4e0f82244b3a4
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-09 17:00:09 -0300

    Adds public ECR repository.

--- efimeral/lib/efimeral-stack.ts
+++ efimeral/lib/efimeral-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
 
 export class EfimeralStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EfimeralQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cfnPublicRepository = new ecr.CfnPublicRepository(this, 'public-efimeral-boxes', {
+      repositoryName: 'efimeral-boxes',
+    });
   }
 }
