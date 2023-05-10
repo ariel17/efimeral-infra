bf682a849b927a24c90371759aff191bf028124f
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-09 21:41:23 -0300

    Adds VPC configuration and tests. Refs #3

--- efimeral/lib/efimeral-stack.ts
+++ efimeral/lib/efimeral-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-export const repositoyName = 'efimeral-boxes';
+
+export const ecrResourceName = 'efimeral-repository';
+export const ecrRepositoyName = 'efimeral-boxes';
+export const vpcResourceName = 'efimeral-boxes-vpc';
+export const vpcPublicSubnetName = 'public-boxes-subnet'
 
 export class EfimeralStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const repository = new ecr.Repository(this, 'efimeral-repository', {
-      repositoryName: repositoyName,
+    const repository = new ecr.Repository(this, ecrResourceName, {
+      repositoryName: ecrRepositoyName,
       imageScanOnPush: true,
+    });
+
+    const vpc = new ec2.Vpc(this, vpcResourceName, {
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: vpcPublicSubnetName,
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
     });
   }
 }
--- efimeral/test/efimeral.test.ts
+++ efimeral/test/efimeral.test.ts
@@ -2,17 +2,52 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as Efimeral from '../lib/efimeral-stack';
 
+const stackName = "MyTestStack"
+
 test('ECR repository created', () => {
   const app = new cdk.App();
     // WHEN
-  const stack = new Efimeral.EfimeralStack(app, 'MyTestStack');
+  const stack = new Efimeral.EfimeralStack(app, stackName);
     // THEN
   const template = Template.fromStack(stack);
 
   template.hasResourceProperties('AWS::ECR::Repository', {
-    RepositoryName: Efimeral.repositoyName,
+    RepositoryName: Efimeral.ecrRepositoyName,
     ImageScanningConfiguration: {
         ScanOnPush: true,
     }
+  });
+
+  template.hasResourceProperties('AWS::EC2::VPC', {
+    Tags: [{
+      Key: "Name",
+      Value: `${stackName}/${Efimeral.vpcResourceName}`
+    }]
+  });
+
+  template.hasResourceProperties('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.0.0/16',
+    MapPublicIpOnLaunch: true,
+    Tags: [{
+      Key: 'aws-cdk:subnet-name',
+      Value: Efimeral.vpcPublicSubnetName,
+    }, {
+      Key: 'aws-cdk:subnet-type',
+      Value: 'Public'
+    }, {
+      Key: "Name",
+      Value: `${stackName}/${Efimeral.vpcResourceName}/${Efimeral.vpcPublicSubnetName}Subnet1`
+    }]
+  });
+
+  template.hasResourceProperties('AWS::EC2::Route', {
+    DestinationCidrBlock: '0.0.0.0/0',
+  });
+
+  template.hasResourceProperties('AWS::EC2::InternetGateway', {
+    Tags: [{
+      Key: "Name",
+      Value: `${stackName}/${Efimeral.vpcResourceName}`
+    }]
   });
 });
