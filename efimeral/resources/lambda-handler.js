ca4f07a6778d0b5576f72bc87694356f6e031415
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-12 16:27:44 -0300

    Removes invalid parameter.

--- efimeral/resources/lambda-handler.js
+++ efimeral/resources/lambda-handler.js
@@ -9,7 +9,6 @@ exports.handler = async (event, context) => {
     networkConfiguration: {
       awsvpcConfiguration: {
         assignPublicIp: 'ENABLED',
-        platformVersion: 'LATEST',
         subnets: [process.env.SUBNET_ID],
         securityGroups: [process.env.SECURITY_GROUP_ID]
       }
