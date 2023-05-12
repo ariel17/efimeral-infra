fb63ff3360d7fa8afb450f034e3cf3218b6c1b46
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-12 13:54:45 -0300

    Reduces all infrastructure to what is required to have a ECR and a dummy lambda.

--- efimeral/lib/efimeral-stack.ts
+++ efimeral/lib/efimeral-stack.ts
@@ -1,34 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
-import * as elb2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import * as path from 'path';
-import * as assets from 'aws-cdk-lib/aws-s3-assets';
 
-
-export const ecrResourceName = 'boxes-repository';
 export const ecrRepositoyName = 'efimeral-boxes';
-export const vpcName = 'public-boxes-vpc';
-export const ecsClusterName = 'boxes-cluster';
-export const ecsClusterInstanceType = 't2.nano';
-export const ecsTaskName = 'box-instantiation';
-export const ecsMinCapacity = 0;
-export const ecsMaxCapacity = 10;
-export const containerName = 'box';
-export const defaultTagImage = 'alpine';
-export const taskCPUs = '256'
-export const taskMemory = '512'
-export const containerCPUs = 1;
-export const containerMemory = 512;
-export const containerStopTimeout = 7200;  // 2 hs
-export const containerPort = 8080;
-export const ecsLoadBalancerName = 'efimeral-load-balancer';
-export const ecsLoadBalancerMemoryLimit = 512;
-export const ecsLoadBalancerCPUs = 256;
+export const lambdaHandler = 'index.handler';
 
 
 export class EfimeralStack extends cdk.Stack {
@@ -41,74 +17,13 @@ export class EfimeralStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const vpc = new ec2.Vpc(this, vpcName, {
-      maxAzs: 3,
-      subnetConfiguration: [
-        {
-          name: vpcName,
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-      ],
-    });
-
-    const cluster = new ecs.Cluster(this, ecsClusterName, {
-      clusterName: ecsClusterName,
-      containerInsights: true,
-      capacity: {
-        instanceType: new ec2.InstanceType(ecsClusterInstanceType),
-        minCapacity: ecsMinCapacity,
-        maxCapacity: ecsMaxCapacity,
-      },
-      vpc: vpc
-    });
-
-    const task = new ecs.TaskDefinition(this, ecsTaskName, {
-      compatibility: ecs.Compatibility.EC2_AND_FARGATE,
-      cpu: taskCPUs,
-      memoryMiB: taskMemory,
-    });
-    task.addContainer(containerName, {
-      image: ecs.ContainerImage.fromEcrRepository(repository, defaultTagImage),
-      cpu: containerCPUs,
-      memoryReservationMiB: containerMemory,
-      portMappings: [
-        {
-          containerPort: containerPort,
-          protocol: ecs.Protocol.TCP,
-        },
-      ],
-    });
-
-    const ecsLoadBalancer = new ecsPatterns.ApplicationLoadBalancedEc2Service(this, ecsLoadBalancerName, {
-      cluster: cluster,
-      taskDefinition: task,
-      cpu: ecsLoadBalancerCPUs,
-      memoryLimitMiB: ecsLoadBalancerMemoryLimit,
-      publicLoadBalancer: true,
-      targetProtocol: elb2.ApplicationProtocol.HTTP,
-      listenerPort: containerPort,
-    });
-    ecsLoadBalancer.targetGroup.configureHealthCheck({
-      enabled: false,
-    });
-
-    const lambdaAsset = new assets.Asset(this, 'lambda-handler.zip', {
-      path: path.join(__dirname, '../resources/lambda-handler.zip'),
-    });
-
     const fn = new lambda.Function(this, 'lambda-handler', {
-      allowPublicSubnet: true,
-      handler: 'index.handler',
-      code: lambda.Code.fromBucket(lambdaAsset.bucket, lambdaAsset.s3ObjectKey),  
       runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('resources'),
+      allowPublicSubnet: true,
+      handler: lambdaHandler,
       environment: {
-        TASK_DEFINITION_ARN: task.taskDefinitionArn,
-        CLUSTER_ARN: cluster.clusterArn,
-        SUBNET_ID: vpc.publicSubnets[0].subnetId,
-        SECURITY_GROUP_ID: vpc.vpcDefaultSecurityGroup,
-        CONTAINER_HARD_LIMIT_TIMEOUT: `${containerStopTimeout}`,
       },
-      vpc,
     });
   }
 }
--- efimeral/test/efimeral.test.ts
+++ efimeral/test/efimeral.test.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import { Template } from 'aws-cdk-lib/assertions';
 import * as Efimeral from '../lib/efimeral-stack';
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 const stackName = "MyTestStack"
 
@@ -19,70 +20,8 @@ test('Stack created', () => {
     }
   });
 
-  template.hasResourceProperties('AWS::EC2::VPC', {
-    Tags: [{
-      Key: "Name",
-      Value: `${stackName}/${Efimeral.vpcName}`
-    }]
-  });
-
-  template.hasResourceProperties('AWS::EC2::Subnet', {
-    CidrBlock: '10.0.0.0/16',
-    MapPublicIpOnLaunch: true,
-    Tags: [{
-      Key: 'aws-cdk:subnet-name',
-      Value: Efimeral.vpcName,
-    }, {
-      Key: 'aws-cdk:subnet-type',
-      Value: 'Public'
-    }, {
-      Key: "Name",
-      Value: `${stackName}/${Efimeral.vpcName}/${Efimeral.vpcName}Subnet1`
-    }]
-  });
-
-  template.hasResourceProperties('AWS::EC2::Route', {
-    DestinationCidrBlock: '0.0.0.0/0',
-  });
-
-  template.hasResourceProperties('AWS::EC2::InternetGateway', {
-    Tags: [{
-      Key: "Name",
-      Value: `${stackName}/${Efimeral.vpcName}`
-    }]
-  });
-
-  template.hasResourceProperties('AWS::ECS::Cluster', {
-    ClusterName: Efimeral.ecsClusterName,
-    ClusterSettings: [{
-      Name: 'containerInsights',
-      Value: 'enabled'
-    }],
-  });
-
-  template.hasResourceProperties('AWS::AutoScaling::LaunchConfiguration', {
-    InstanceType: Efimeral.ecsClusterInstanceType,
-  });
-
-  template.hasResourceProperties('AWS::AutoScaling::AutoScalingGroup', {
-    MinSize: `${Efimeral.ecsMinCapacity}`,
-    MaxSize: `${Efimeral.ecsMaxCapacity}`,
-  });
-
-  template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-    Cpu: Efimeral.taskCPUs,
-    Memory: Efimeral.taskMemory,
-    RequiresCompatibilities: ["FARGATE"],
-    ContainerDefinitions: [{
-        Name: Efimeral.containerName,
-        Cpu: Efimeral.containerCPUs,
-        Essential: true,
-        MemoryReservation: Efimeral.containerMemory,
-        PortMappings: [{
-            ContainerPort: Efimeral.containerPort,
-            Protocol: ecs.Protocol.TCP,
-        }],
-        StopTimeout: Efimeral.containerStopTimeout,
-    }]
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Handler: Efimeral.lambdaHandler,
+    Runtime: 'nodejs16.x',
   });
 });
