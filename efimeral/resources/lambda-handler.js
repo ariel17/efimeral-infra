27a44ec5913734f31d4846138f66c893ffe54501
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-14 20:42:57 -0300

    Obtains container public DNS instead of public IP :)

--- efimeral/resources/lambda-handler.js
+++ efimeral/resources/lambda-handler.js
@@ -1,43 +1,22 @@
 const AWS = require('aws-sdk');
 const ecs = new AWS.ECS();
+const ec2 = new AWS.EC2();
 
 
 exports.handler = async (event, context) => {
   console.log(`Env parameters: ${JSON.stringify(process.env)}`);
 
-  const taskParams = {
-    taskDefinition: process.env.TASK_DEFINITION_ARN, 
-    cluster: process.env.CLUSTER_ARN,
-    launchType: 'FARGATE',
-    networkConfiguration: {
-      awsvpcConfiguration: {
-        assignPublicIp: 'ENABLED',
-        subnets: [process.env.SUBNET_ID],
-        securityGroups: [process.env.SECURITY_GROUP_ID]
-      }
-    },
-    count: 1,
-    startedBy: 'lambda-function'
-  };
-  console.log(`Creating task... parameters: ${JSON.stringify(taskParams)}`);
 
   try {
-    const runTaskData = await ecs.runTask(taskParams).promise();
-    console.log(`Task executed: ${JSON.stringify(runTaskData)}`);
-  
-    const waitParams = {
-      cluster: process.env.CLUSTER_ARN,
-      tasks: [runTaskData['tasks'][0]['taskArn'],]
-    }
-    console.log(`Checking task running state... parameters: ${JSON.stringify(waitParams)}`);
-
-    const waitData = await ecs.waitFor('tasksRunning', waitParams).promise();
-    console.log(`Task is in RUNNING state: ${JSON.stringify(waitData)}`);
+    const runTaskData = runTask();
+    const waitData = waitForRunningState(runTaskData.tasks[0].taskArn);
+    const containerURL = getContainerURL(waitData.tasks[0].attachments[0].details);
 
     return {
       statusCode: 201,
       body: JSON.stringify({
         message: 'Container created',
+        url: containerURL,
       }),
     };
 
@@ -52,3 +31,59 @@ exports.handler = async (event, context) => {
     };
   };
 };
+
+async function createTask() {
+  const taskParams = {
+    taskDefinition: process.env.TASK_DEFINITION_ARN, 
+    cluster: process.env.CLUSTER_ARN,
+    launchType: 'FARGATE',
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        assignPublicIp: 'ENABLED',
+        subnets: [process.env.SUBNET_ID],
+        securityGroups: [process.env.SECURITY_GROUP_ID]
+      }
+    },
+    count: 1,
+    startedBy: 'lambda-function'
+  };
+
+  const runTaskData = await ecs.runTask(taskParams).promise();
+  console.log(`Task executed: ${JSON.stringify(runTaskData)}`);
+ 
+  return runTaskData;
+}
+
+async function waitForRunningState(taskArn) {
+  const waitParams = {
+    cluster: process.env.CLUSTER_ARN,
+    tasks: [taskArn,]
+  }
+
+  const waitData = await ecs.waitFor('tasksRunning', waitParams).promise();
+  console.log(`Task is in RUNNING state: ${JSON.stringify(waitData)}`);
+
+  return waitData;
+}
+
+async function getContainerURL(taskAttachmentDetails) {
+  let eni = '';
+  for (let i = 0; i < tasksAttachmentDetails.length; i++) {
+    if (taskAttachmentDetails[i].name === 'networkInterfaceId') {
+      eni = taskAttachmentDetails[i].value;
+      break;
+    }
+  }
+  
+  if (eni === '') {
+    throw 'Cannot obtain network interface ID';
+  }
+  
+  const networkParams = {
+    NetworkInterfaceIds: [eni],
+  }
+  const networkData = await ec2.describeNetworkInterfaces(networkParams).promise();
+  console.log(`Network data: ${JSON.stringify(networkData)}`);
+  
+  return `http://${networkData.NetworkInterfaces[0].Association.PublicDnsName}:${process.env.CONTAINER_PORT}`
+}
