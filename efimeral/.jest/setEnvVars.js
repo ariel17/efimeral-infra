4ae8cbee40b1e9fbb9cfecc8c3f4005cca6e977f
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-16 15:25:03 -0300

    Adds env vars and matchers.

---
+++ efimeral/.jest/setEnvVars.js
@@ -0,0 +1,1 @@
+process.env.CONTAINER_PORT=8080
--- efimeral/jest.config.js
+++ efimeral/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.js'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
 };
