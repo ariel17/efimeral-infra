485729ef1201f426d88be34712741e753876fd01
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-16 15:27:42 -0300

    Adds lambda test unit. Closes #6

---
+++ efimeral/test/lambda-handler.test.js
@@ -0,0 +1,72 @@
+const { ECSClient, RunTaskCommand, DescribeTasksCommand } = require("@aws-sdk/client-ecs");
+const { EC2Client, DescribeNetworkInterfacesCommand } = require("@aws-sdk/client-ec2");
+const { mockClient } = require("aws-sdk-client-mock");
+
+const lambdaFunction = require("../resources/lambda-handler");
+
+const ecsMock = mockClient(ECSClient);
+const ec2Mock = mockClient(EC2Client);
+
+describe("Create containers", () => {
+  beforeEach(() => {
+  });
+
+  afterEach(() => {
+	ecsMock.reset();
+	ec2Mock.reset();
+  });
+
+  test("should create container successfully", async () => {
+    ecsMock.on(RunTaskCommand).resolves({
+		tasks: [{
+			taskArn: 'fakeARN',
+		}]});
+	ecsMock.on(DescribeTasksCommand).resolves({
+		tasks: [{
+			attachments: [{
+				details: [{
+					name: 'networkInterfaceId',
+					value: 'fakeNetworkId',
+				}]
+			}],
+			lastStatus: 'RUNNING',
+			desiredStatus: 'RUNNING',
+		}],
+	});
+	ec2Mock.on(DescribeNetworkInterfacesCommand).resolves({
+		NetworkInterfaces: [{
+			Association: {
+				PublicDnsName: 'publicDNS',
+			},
+		}],
+	});
+
+    const result = await lambdaFunction.handler();
+    expect(result).toStrictEqual({
+		"body": "{\"message\":\"Container created\",\"url\":\"http://publicDNS:8080\"}", 
+		"statusCode": 201
+	});
+  });
+
+  test("should fail if network ID is not found", async () => {
+    ecsMock.on(RunTaskCommand).resolves({
+		tasks: [{
+			taskArn: 'fakeARN',
+		}]});
+	ecsMock.on(DescribeTasksCommand).resolves({
+		tasks: [{
+			attachments: [{
+				details: []
+			}],
+			lastStatus: 'RUNNING',
+			desiredStatus: 'RUNNING',
+		}],
+	});
+
+    const result = await lambdaFunction.handler();
+    expect(result).toStrictEqual({
+		"body": "{\"message\":\"Error creating container\",\"error\":\"Cannot obtain network interface ID\"}",
+		"statusCode": 500
+	});
+  });
+});
