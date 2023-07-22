fa055a36a3e234d2146429d35c476c489fe62412
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-07-22 20:10:31 -0300

    Adds custom eventbus to infrastructure and DNS configuration for boxes subdomain.

--- efimeral/lib/constructs/infrastructure.ts
+++ efimeral/lib/constructs/infrastructure.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as servicediscovery from "aws-cdk-lib/aws-servicediscovery";
+import * as events from "aws-cdk-lib/aws-events";
 
 
 export interface InfrastructureProps {
@@ -10,6 +12,7 @@ export interface InfrastructureProps {
   readonly maxAzs: number;
   readonly clusterEc2MinCapacity: number;
   readonly clusterEc2MaxCapacity: number;
+  readonly boxesSubDomain: string;
 }
 
 export class Infrastructure extends Construct {
@@ -18,6 +21,9 @@ export class Infrastructure extends Construct {
     public readonly vpc: ec2.Vpc;
     public readonly sg: ec2.SecurityGroup;
     public readonly cluster: ecs.Cluster;
+    public readonly bus: events.EventBus;
+    public readonly ns: servicediscovery.PublicDnsNamespace;
+    public readonly service: servicediscovery.Service;
 
     constructor(scope: Construct, id: string, props: InfrastructureProps) {
         super(scope, id);
@@ -62,5 +68,21 @@ export class Infrastructure extends Construct {
           vpc: vpc,
         }); 
         this.cluster = cluster;
+
+        const bus = new events.EventBus(this, 'pending-domains-bus', {
+          eventBusName: 'pending-domain-bus'
+        });
+        this.bus = bus;
+
+        const ns = new servicediscovery.PublicDnsNamespace(this, 'boxes-ns', {
+          name: props.boxesSubDomain,
+          description: 'Namespace for boxes',
+        });
+        this.ns = ns;
+  
+        const service = new servicediscovery.Service(this, 'boxes-ns-service', {
+          namespace: ns,
+        });
+        this.service = service;
     }
 }
